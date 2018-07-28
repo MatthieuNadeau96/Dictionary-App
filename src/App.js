22df2c50675b64cc707400eebb8f168d0bc4446c
Matthieu <matthieunadeau96@gmail.com>
2018-07-28 16:11:17 -0400

    Added API call and started styling the list

--- src/App.js
+++ src/App.js
@@ -2,11 +2,63 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+
+    fetch('http://api.pearson.com/v2/dictionaries/entries?headword=dog')
+    .then(response => response.json())
+    // .then(parsedJSON => console.log(parsedJSON.results))
+    .then(parsedJSON => parsedJSON.results.map(data => (
+      {
+        headword: `${data.headword}`,
+        id: `${data.id}`,
+        part_of_speech: `${data.part_of_speech}`,
+        definition: `${data.senses["0"].definition}`
+      }
+    )))
+    .then(list => this.setState({ list, isLoading: false }))
+    .catch(error => console.log('parsing failed', error))
+
+  }
+
   render() {
+    const {isLoading, list} = this.state;
     return (
       <div className="container">
         <h1>Dictionary</h1>
         <input type="text"></input>
+
+        <div className="search-list">
+          <h2>Search Results</h2>
+          {
+
+            list.map(word => {
+              const { headword, id, part_of_speech, definition } = word;
+              return (
+                <div className="wordCard" key={id}>
+                  <div className="top">
+                    <p className="title">{headword}</p>
+                    <p className="part-of-speech">{part_of_speech}</p>
+                  </div>
+                  <p className="definition">{definition}</p>
+                </div>
+              )
+          })
+        }
+        </div>
+
         <h4><a href="/">Matthieu Nadeau</a></h4>
       </div>
     );
