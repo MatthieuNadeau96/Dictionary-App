f7c33f2623464af2085aa2bab856fdf3b053143e
Matthieu <matthieunadeau96@gmail.com>
2018-07-28 13:26:27 -0400

    Added some simple UI Design

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi</h1>
+      <div className="container">
+        <h1>Dictionary</h1>
+        <input type="text"></input>
+        <h4><a href="/">Matthieu Nadeau</a></h4>
       </div>
     );
   }
